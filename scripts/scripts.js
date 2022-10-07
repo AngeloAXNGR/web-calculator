69836c635916be2a28bdcbd970b8517683bec06e
LuhxEn <axngrsantos@gmail.com>
2022-10-07 11:47:40 +0800

    Add basic math functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,8 @@
 const numberButtons = document.querySelectorAll('.num-buttons');
 const operations = document.querySelector('#operations');
-const result = document.querySelector('#final-result')
-
-
-
-
-// Get the TEXT value of num-buttons and display on output
-numberButtons.forEach(button =>{
-  button.addEventListener('click', (e) =>{
-    result.textContent += e.target.value;
-  })
-})
-
-// Get the TEXT value of opr-buttons and display on output
-operatorButtons = document.querySelectorAll('.opr-buttons');
-operatorButtons.forEach(button =>{
-  button.addEventListener('click', (e)=>{
-    result.textContent += e.target.value;
-  });
-});
+const displayValue = document.querySelector('#final-result')
+let result = displayValue;
+let values = [];
 
 
 // Clear and Delete Functionalities
@@ -36,3 +20,72 @@ deleteButton.addEventListener('click', deleteText);
 function deleteText(){
   result.textContent = result.textContent.slice(0, -1);
 }
+
+
+
+// Get the TEXT value of num-buttons and display on screen
+numberButtons.forEach(button =>{
+  button.addEventListener('click', (e) =>{
+
+    displayValue.textContent += e.target.value;
+  })
+})
+
+// Get the TEXT value of opr-buttons and display on screen
+operatorButtons = document.querySelectorAll('.opr-buttons');
+operatorButtons.forEach(button =>{
+  button.addEventListener('click', (e)=>{
+    values.push(displayValue.textContent);
+    operations.textContent += ' ' + displayValue.textContent + ' ' + e.target.value;
+    displayValue.textContent = '';
+    values.push(e.target.value);
+    console.log(values);
+
+    if(e.target.value === '='){
+      operate();
+    }
+  });
+});
+
+
+
+
+function operate(){
+  let num1 = Number(values[0]);
+  let num2 = Number(values[2]);
+  let operator = values[1];
+  if(operator === '+'){
+    let operation = add(num1, num2);
+    displayValue.textContent = operation;
+  }else if(operator === '-'){
+    let operation = subtract(num1, num2);
+    displayValue.textContent = operation;
+  }else if(operator === 'x'){
+    let operation = multiply(num1, num2);
+    displayValue.textContent = operation;
+  }else if(operator === '/'){
+    let operation = divide(num1, num2);
+    displayValue.textContent = operation;
+  }
+  console.log(values);
+}
+
+function add(a, b){
+  return a + b;
+}
+
+function subtract(a,b){
+  return a - b;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a / b
+}
+
+
+
+
